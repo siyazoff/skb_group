19d6598e81d9af17f5ca1f4afcb0dfe7baf2732b
Darkhan <dms18@tpu.ru>
2023-11-15 13:59:10 +0600

    add input file reader

--- js/main.js
+++ js/main.js
@@ -20,4 +20,20 @@ $(document).ready(function () {
   $(".humb").on("click", function () {
     $(".header").toggleClass("header__active");
   });
+
+  let inputs = document.querySelectorAll(".input__file");
+  Array.prototype.forEach.call(inputs, function (input) {
+    let label = input.nextElementSibling,
+      labelVal = label.querySelector(".input__file-button-text").innerText;
+
+    input.addEventListener("change", function (e) {
+      let countFiles = "";
+      if (this.files && this.files.length >= 1) countFiles = this.files.length;
+
+      if (countFiles)
+        label.querySelector(".input__file-button-text").innerText =
+          "Выбрано файлов: " + countFiles;
+      else label.querySelector(".input__file-button-text").innerText = labelVal;
+    });
+  });
 });
